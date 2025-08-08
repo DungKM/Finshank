6acb7518b8abea9894b4d1ca74d34f326cb0198d
DungKM <hoanganhdung13122000@gmail.com>
2025-08-08 07:21:22 +0700

    feat: Comment create

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -53,6 +53,11 @@ namespace api.Repository
 
         }
 
+        public Task<bool> StockExists(int id)
+        {
+           return _context.Stock.AnyAsync(x => x.Id == id);
+        }
+
         public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
         {
             var existingStock = await _context.Stock.FirstOrDefaultAsync(x => x.Id == id);
