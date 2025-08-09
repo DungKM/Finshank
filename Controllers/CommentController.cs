eaff7b0aca0d3f21d0d74338336daa1b8cb3d6e9
DungKM <hoanganhdung13122000@gmail.com>
2025-08-09 07:23:39 +0700

    feat: Delete comments

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -50,7 +50,29 @@ namespace api.Controllers
             var commentModel = commentDto.ToCommentFromCreate(stockId);
             await _commentRepo.CreateAsync(commentModel);
             return CreatedAtAction(nameof(GetById), new { id = commentModel.Id }, commentModel.ToCommentDto());
-
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
+        {
+            var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
+            if (comment == null)
+            {
+                NotFound("Comment not found.");
+            }
+            return Ok(comment.ToCommentDto());
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var comment = await _commentRepo.GetByIdAsync(id);
+            if (comment == null)
+            {
+                return NotFound("Comment not found.");
+            }
+            await _commentRepo.DeleteAsync(id);
+            return NoContent();
         }
     }
 }
--- Repository/CommentRepository.cs
+++ Repository/CommentRepository.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using api.Data;
 using api.Interfaces;
 using api.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
@@ -27,6 +29,19 @@ namespace api.Repository
             return commentModel;
         }
 
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _context.Comment.FirstOrDefaultAsync(x => x.Id == id);
+            if (commentModel == null)
+            {
+                return null; // Comment not found
+            }
+            _context.Comment.Remove(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
         public async Task<List<Comment>> GetAllAsync()
         {
             return await _context.Comment.ToListAsync();
@@ -36,6 +51,23 @@ namespace api.Repository
         {
             return _context.Comment.FirstOrDefaultAsync(x => x.Id == id);
         }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            var existingComment = await _context.Comment.FindAsync(id);
+            if (existingComment == null)
+            {
+                return null; // Comment not found
+            }
+            existingComment.Title = commentModel.Title;
+            existingComment.Content = commentModel.Content;
+
+            await _context.SaveChangesAsync();
+            return existingComment;
+        }
+
+
+
     }
 
 }
